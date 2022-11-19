06f0c038eff73f952f36a520adac986ffade3495
Brilian <hudabrilian@gmail.com>
2022-11-19 13:05:47 +0700

    moved links to constants file

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    name: "Instagram",
+    url: "https://instagram.com/hudabrilians",
+  },
+  //   {
+  //     name: "ma luv Instagram",
+  //     url: "https://instagram.com/pomi_nar",
+  //   },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/hudabrilian/",
+  },
+  {
+    name: "Github",
+    url: "https://github.com/hudabrilian",
+  },
+  {
+    name: "Twitter",
+    url: "https://twitter.com/hudabrilian",
+  },
+];
